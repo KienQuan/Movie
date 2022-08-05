aebf9a669c5c64b219fc6e9819aa4f2b8e027007
KienQuan <102026057+KienQuan@users.noreply.github.com>
2022-08-05 23:45:14 +0700

    version_2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
+import { createBrowserHistory } from 'history';
 import './App.css';
 import {Switch, Router} from 'react-router';
+import { UserTemplate } from './templates/UserTemplate/UserTemplate';
+import Login from './pages/Login/Login';
+
+export const history = createBrowserHistory();
+
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
-
+      <UserTemplate path="/login" exact Component={Login} />
       </Switch>
     </Router>
   );
